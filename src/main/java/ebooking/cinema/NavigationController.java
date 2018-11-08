7bb95c93341018b10be2a8b3680d24a474bf8d65
Tripp <tripp.guinn@outlook.com>
2018-11-08 18:58:25 -0500

    Navigation
    Added Navigation Controller + more Thymeleaf Markup

--- src/main/java/ebooking/cinema/NavigationController.java
+++ src/main/java/ebooking/cinema/NavigationController.java
@@ -1,8 +1,14 @@
 package ebooking.cinema;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NavigationController {
+
+    @GetMapping("/index")
+    public String goHome() {
+        return "index";
+    }
 
 }
--- src/main/java/ebooking/cinema/User.java
+++ src/main/java/ebooking/cinema/User.java
@@ -4,53 +4,63 @@ import java.sql.*;
 
 public class User {
 
-    String username;
+    String email;
     String password;
+    boolean loggedIn = false;
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public boolean getLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
     public boolean login() {
 
-                try{
-                    Connection  myConn=  DriverManager.getConnection("jdbc:mysql://localhost:3306/cinema2.0?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","password");
-                    Statement  myStatment=  myConn.createStatement();
-                    ResultSet  myResult=  myStatment.executeQuery("SELECT *  FROM user");
-                    while(myResult.next()){
-                        System.out.println(myResult.getString("userId")+" "+myResult.getString("emailAddress")+" "+myResult.getString("firstName")+" "+myResult.getString("lastName"));
-                    } //while
-                    System.out.println("Connected!");
-                }
+        try{
+            Connection  myConn=  DriverManager.getConnection("jdbc:mysql://localhost:3306/cinema2.0?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","LegendOfLink30");
+            Statement  myStatment=  myConn.createStatement();
+            ResultSet  myResult=  myStatment.executeQuery("SELECT *  FROM user WHERE emailAddress = '" + this.email + "' AND  password = '" + this.password + "'");
+            while(myResult.next()){
+                System.out.println(myResult.getString("userId")+" "+myResult.getString("emailAddress")+" "+myResult.getString("firstName")+" "+myResult.getString("lastName"));
 
-                catch(Exception  ex){
-                    ex.printStackTrace();
-                }
+                RegUser regUser = new RegUser();
+                regUser.email = this.email;
+                regUser.fname = myResult.getString("firstName");
+                regUser.lname = myResult.getString("lastName");
+            }
+            loggedIn = true;
+            System.out.println("Connected!");
+        }
 
-        return false;
+        catch(Exception  ex){
+            ex.printStackTrace();
+        }
+
+        return loggedIn;
     }
 }
 
 class RegUser extends User {
-    String email;
     String fname;
     String lname;
-
-    public String getEmail() {
-        return email;
-    }
 
     public String getFname() {
         return fname;
@@ -60,10 +70,6 @@ class RegUser extends User {
         return lname;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public void setFname(String fname) {
         this.fname = fname;
     }
@@ -71,4 +77,8 @@ class RegUser extends User {
     public void setLname(String lname) {
         this.lname = lname;
     }
+}
+
+class Admin extends RegUser {
+
 }
